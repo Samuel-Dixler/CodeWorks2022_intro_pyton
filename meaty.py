bdd23a1547d28ef6b0299824e02371307f8a3b61
Samuel Dixler <dixler.inc@gmail.com>
2022-07-12 11:01:10 -0400

    done!

--- meaty.py
+++ meaty.py
@@ -1,8 +1,11 @@
 ans = input('Do you eat meat? (y/n):')
-
+#asks user to input a y or an n
 if ans == 'y':
     print("Here is the meat menu...")
 elif ans == 'n':
     print("Here is the veggie menu...")
+    #checks if user inputed a y or an n and then outputs
+
 else:
     print("Sorry! didn't recognize your response")
+#if inputed anything other than y or n
